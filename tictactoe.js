a15817d78b38bb25060d798e9270348d824eca66
leahphuong <haphuong.ng@gmail.com>
2015-10-14 15:31:10 -0400

    redo wireframe, use <div> instead of <button> and add jQuery to JS

--- tictactoe.js
+++ tictactoe.js
@@ -1,54 +1,70 @@
-  var buttons = [button1, button2, button3, button4, button5, button6, button7, button8, button9];
-  var gameover = false;
-  function checkCombination(b1,b2,b3){
-    if(b1.innerHTML != '' && b1.innerHTML == b2.innerHTML && b1.innerHTML == b3.innerHTML){
+'use strict'
+
+var button = [];
+for(var i=0; i<9;i++){
+  button[i] = $('#button'+(i+1));
+}
+  function displayBoard(x){
+    return $('#boardMsg').text(x);}
+var gameover = false;
+
+function checkCombination(b1,b2,b3){
+    if(b1.html() != '' && b1.html() == b2.html() && b1.html() == b3.html()){
       gameover = true;
-      b1.style.backgroundColor = '#FFCCFF';
-      b2.style.backgroundColor = '#FFCCFF';
-      b3.style.backgroundColor = '#FFCCFF';
+      if(b1.html() == 'X'){
+      displayBoard("X won!");
+    }else{
+      displayBoard("O won!");
+    }
+      b1.css('background-color', 'blue');
+      b2.css('background-color', 'blue');
+      b3.css('background-color', 'blue');
     }
   }
   function checkWin(){
-    checkCombination(button1,button2,button3);
-    checkCombination(button4,button5,button6);
-    checkCombination(button7,button8,button9);
-    checkCombination(button1,button4,button7);
-    checkCombination(button2,button5,button8);
-    checkCombination(button3,button6,button9);
-    checkCombination(button1,button5,button9);
-    checkCombination(button3,button5,button7);
-
+    checkCombination(button[0],button[1],button[2]);
+    checkCombination(button[3],button[4],button[5]);
+    checkCombination(button[6],button[7],button[8]);
+    checkCombination(button[0],button[3],button[6]);
+    checkCombination(button[1],button[4],button[7]);
+    checkCombination(button[2],button[5],button[8]);
+    checkCombination(button[0],button[4],button[8]);
+    checkCombination(button[2],button[4],button[6]);
   }
+
   function pickSquare(){
-    if(gameover || this.innerHTML !== '') return;
-    this.innerHTML = selectTurn.value;
-    if(this.innerHTML == 'X'){
+    var $this = $(this);
+    if(gameover || $this.html() !== '') return;
+    $this.html(selectTurn.value);
+    if($this.html() == 'X'){
       selectTurn.value = 'O';
     } else {
       selectTurn.value = 'X'
     }
     checkWin();
   }
-  button1.onclick = pickSquare;
-  button2.onclick = pickSquare;
-  button3.onclick = pickSquare;
-  button4.onclick = pickSquare;
-  button5.onclick = pickSquare;
-  button6.onclick = pickSquare;
-  button7.onclick = pickSquare;
-  button8.onclick = pickSquare;
-  button9.onclick = pickSquare;
+
+  $('#button1').on('click', pickSquare);
+  $('#button2').on('click', pickSquare);
+  $('#button3').on('click', pickSquare);
+  $('#button4').on('click', pickSquare);
+  $('#button5').on('click', pickSquare);
+  $('#button6').on('click', pickSquare);
+  $('#button7').on('click', pickSquare);
+  $('#button8').on('click', pickSquare);
+  $('#button9').on('click', pickSquare);
 
   function resetGame(){
-    for(var i = 0; i < buttons.length; i++){
-      buttons[i].innerHTML = '';
-    }
-
-    for(var i = 0; i < buttons.length; i++){
-      buttons[i].style.backgroundColor = '';
-    }
-    gameover = false;
+    /*for(var i = 0; i < buttons.length; i++){*/
+      $('.button').css('background-color','');
+      $('.button').html('');
+     /*for(var i = 0; i < buttons.length; i++){
+      buttons[i].css('backgroundColor', '');
+    }*/
+      gameover = false;
   }
-  playButton.onclick = resetGame;
+
+
+  $('#playButton').on('click', resetGame);
 
 
